3bd8809e5fe8a0edb29b7eedb7add99b2efa650f
b115030 <iitiangokul@gmail.com>
2020-10-14 20:16:06 +0530

    Ability to add multiple Persons added

--- src/com/AddressBook/AddressBookMain.java
+++ src/com/AddressBook/AddressBookMain.java
@@ -10,32 +10,35 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
 
-
-        int option;
+    	int option;
         Scanner readIn = new Scanner(System.in);
         try {
             Utils util = new Utils();
-            System.out.println("1. To add a new person \n2. To update a person \n3. Delete a Person \n4. Exit");
-            option = readIn.nextInt();
-            switch (option) {
-                case 1:
-                    Person person = new Person();
-                    util.showAddressBook();
-                    break;
-                case 2:
-                    util.showAddressBook();
-                    util.updatePerson();
-                    util.showAddressBook();
-                    break;
-                case 3:
-                	util.showAddressBook();
-                	util.deletePerson();
-                	util.showAddressBook();
-                	break;
-                
-                case 4:
-                    break;
-
+            while(true) {
+            	boolean Flag=true;
+            	System.out.println("1. To add a new person \n2. To update a person \n3. Delete a Person \n4. Exit");
+            	option = readIn.nextInt();
+            	switch (option) {
+                	case 1:
+                		Person person = new Person();
+                		util.showAddressBook();
+                		break;
+                	case 2:
+                		util.showAddressBook();
+                		util.updatePerson();
+                		util.showAddressBook();
+                		break;
+                	case 3:
+                		util.showAddressBook();
+                		util.deletePerson();
+                		util.showAddressBook();
+                		break;
+                	case 4:
+                		Flag=false;
+                		break;
+            	}
+            	if (Flag==false)
+            		break;
             }
         } catch (IOException e) {
             e.printStackTrace();
